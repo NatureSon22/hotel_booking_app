cb17516fa8c160df71821b4c424e0f85167a08dd
NatureSon22 <blendingworker@gmail.com>
2024-07-22 15:18:22 +0800

    edited cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,12 +10,7 @@ import path from "path";
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // serve the static files on the root of the server
 app.use(express.static(path.join(__dirname, "../../client/dist")));
